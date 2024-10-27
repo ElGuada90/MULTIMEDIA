c5702de1d48cce928d8a65c97b1ee420bfa90e5d
ElGuada90 <guadamuz1977@gmial.com>
2024-10-27 16:30:03 -0500

    actualizacion de fromulario de login, se le agregaron iconos..

--- supermovies/views.py
+++ supermovies/views.py
@@ -13,6 +13,17 @@ def home():
         message = "Top Stream ",
         content =  formatted_now)
     
+    
+@app.route('/login')
+def login():
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %I:%M:%S %p")
+    return render_template(
+        "login.html",
+        title = "EG90 Series",
+        message = "Top Series ", 
+        content = formatted_now)    
+    
 @app.route('/series')
 def series():
     now = datetime.now()
